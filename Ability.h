68ad880e83592d79c073f772fc58e6d586f6b72c
Saima Yunus <saimayunus925@gmail.com>
2023-12-31 01:27:55 -0800

    changed 'type' variable in Ability class to an enum

--- Ability.h
+++ Ability.h
@@ -9,15 +9,17 @@
 #include <string>
 using std::string;
 
+enum AbilityType { offensive, defensive, utility };
+
 class Ability {
     protected:
-        string name; // The name of the ability
-        string description; // A brief description of what the ability does
-        string type; // The type of ability (e.g., offensive, defensive, utility)
-        int cooldown; // The time it takes before the ability can be used again
-        int mana_cost; // If applicable, the amount of mana or energy required to use the ability
-        int range; // The effective range of the ability
-        int damage_or_healing; // The amount of damage dealt or healing provided
+        string ability_name; // The name of the ability
+        string ability_description; // A brief description of what the ability does
+        enum AbilityType ability_type; // The type of ability (e.g., offensive, defensive, utility)
+        int ability_cooldown; // The time it takes before the ability can be used again
+        int ability_mana_cost; // If applicable, the amount of mana or energy required to use the ability
+        int ability_range; // The effective range of the ability
+        int ability_damage_or_healing; // The amount of damage dealt or healing provided
     public:
         // TODO - make Ability class methods
 };
